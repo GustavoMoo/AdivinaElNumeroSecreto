4a0ed77d22a8f0623991d916dade1cc015d05e66
Gustavo Adolfo Moo Contreras <16470266@itcampeche.edu.mx>
2017-05-19 00:42:10 -0500

    Version final del projecto.

--- src/adivinaelnumerosecreto/AdivinaElNumeroSecretoMain.java
+++ src/adivinaelnumerosecreto/AdivinaElNumeroSecretoMain.java
@@ -5,17 +5,41 @@
  */
 package adivinaelnumerosecreto;
 
+import java.util.Scanner;
+import modelo.Juego;
+
 /**
  *
  * @author gusta
  */
-public class AdivinaElNumeroSecreto {
+
+    public class AdivinaElNumeroSecretoMain {
 
     /**
      * @param args the command line arguments
      */
+        
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+ Scanner entrada = new Scanner(System.in);
+ int numero;
+ boolean seAdivinoElNumeroSecreto = false;
+ Juego juego = new Juego(1, 100, 5);
+ while (juego.puedeIntentarDeNuevo() == true
+ && !seAdivinoElNumeroSecreto) {
+ System.out.print("Adivine el número secreto: ");
+ numero = entrada.nextInt();
+ seAdivinoElNumeroSecreto = juego.esElNumeroSecreto(numero);
+ if (seAdivinoElNumeroSecreto) {
+ System.out.println("Felicidades. Has adivinado el número secreto.");
+ }
+ else {
+ System.out.println("Fallaste. El número secreto es "
+ + juego.getTextoDeAyuda(numero)
++ " que el número propocionado");
+ System.out.println("Intenta de nuevo.");
+ }
+ }
+ System.out.println("El número secreto es " + juego.getNumeroSecreto());
+ }
     
 }
